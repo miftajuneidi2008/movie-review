d7a7d79e63d6671caf5f566913396f25f2ac2758
miftajuneidi2008 <miftajuneidi2008@gmail.com>
2024-07-11 11:26:36 +0300

    updated

--- app/favorite/page.jsx
+++ app/favorite/page.jsx
@@ -1,19 +1,10 @@
-"use client"
-import React from 'react'
-import { redirect } from 'next/navigation';
-import { UserAuth } from '@/context/AuthContext';
-import { useRouter } from 'next/navigation';
 
+import React from 'react'
 const FavoriteList = () => {
- const {user} = UserAuth()
- const router = useRouter()
- if(!user)
- {
-  router.push('/login')
- }
+
   return (
     <div>
-      <p>hello </p>
+      <p>Favorite movie </p>
     </div>
   )
 }
